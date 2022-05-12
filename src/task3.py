9dac18d7d1d966fe19470bda2408384b87bc03c6
Heather <heather@carraig.ai>
2022-05-12 17:38:14 +0100

    some refactoring

--- src/task3.py
+++ src/task3.py
@@ -5,6 +5,76 @@ from pyspark.ml.feature import VectorAssembler, StringIndexer, IndexToString
 import argparse
 from pyspark.sql.types import StructType, DoubleType, StringType
 
+FEATURE_COLS = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+
+
+def run(master_config, input_file_name, predictions_output):
+
+    spark = SparkSession.builder \
+        .appName("Part_3") \
+        .master(master_config) \
+        .getOrCreate()
+
+    df = load_df(input_file_name, spark)
+
+    assembler, lr_model, label_indexer = train_model(df)
+
+    ## Test the label_indexer with data
+    result = get_predictions(lr_model, label_indexer, assembler.transform(spark.createDataFrame(
+        [(5.1, 3.5, 1.4, 0.2),
+         (6.2, 3.4, 5.4, 2.3)], FEATURE_COLS)))
+
+    write_predictions(predictions_output, result)
+
+
+def train_model(df):
+
+    assembler = VectorAssembler(inputCols=FEATURE_COLS, outputCol='features')
+
+    training_data = assembler.transform(df)
+    string_indexer = StringIndexer(inputCol="class", outputCol="label", handleInvalid="error",
+                                   stringOrderType="frequencyDesc")
+    label_indexer = string_indexer.fit(training_data)
+    td = label_indexer.transform(training_data)
+
+    # Train the label_indexer
+    lr = LogisticRegression(labelCol="label", family="multinomial", regParam=1e5)
+    lr_model = lr.fit(td)
+
+    return assembler, lr_model, label_indexer
+
+
+def load_df(input_file_name, spark):
+
+    schema = StructType() \
+        .add("sepal_length", DoubleType(), True) \
+        .add("sepal_width", DoubleType(), True) \
+        .add("petal_length", DoubleType(), True) \
+        .add("petal_width", DoubleType(), True) \
+        .add("class", StringType(), True)
+
+    df = spark.read.format("csv") \
+        .options(header="False") \
+        .schema(schema) \
+        .load(input_file_name)
+
+    return df
+
+
+def get_predictions(lr_model, model, test_data):
+
+    predictions = lr_model.transform(test_data)
+    inverter = IndexToString(inputCol="prediction", outputCol="prediction_label", labels=model.labels)
+    inverted = inverter.transform(predictions)
+    return inverted.select("prediction_label").collect()
+
+
+def write_predictions(predictions_output, result):
+    with open(predictions_output, 'w') as f:
+        for row in result:
+            f.write(f'{row.prediction_label} \n')
+
+
 if __name__ == "__main__":
 
     ## Part 3 - LogisticRegression
@@ -20,51 +90,4 @@ if __name__ == "__main__":
     master_config = args["master"]
     predictions_output = args["predictions_output"]
 
-    spark = SparkSession.builder \
-        .appName("Part_3") \
-        .master(master_config) \
-        .getOrCreate()
-
-    schema = StructType() \
-        .add("sepal_length", DoubleType(), True) \
-        .add("sepal_width", DoubleType(), True) \
-        .add("petal_length", DoubleType(), True) \
-        .add("petal_width", DoubleType(), True) \
-        .add("class", StringType(), True)
-
-    df = spark.read.format("csv") \
-        .options(header="False") \
-        .schema(schema) \
-        .load(input_file_name)
-
-    assembler = VectorAssembler(
-        inputCols=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'],
-        outputCol='features')
-
-    training_data = assembler.transform(df)
-
-    stringIndexer = StringIndexer(inputCol="class", outputCol="label", handleInvalid="error",
-                                  stringOrderType="frequencyDesc")
-
-    model = stringIndexer.fit(training_data)
-    td = model.transform(training_data)
-
-    # Build the model
-    lr = LogisticRegression(labelCol="label", family="multinomial", regParam=1e5)
-    lrModel = lr.fit(td)
-
-    ## Test the model with data
-    test_data = assembler.transform(spark.createDataFrame(
-            [(5.1, 3.5, 1.4, 0.2),
-            (6.2, 3.4, 5.4, 2.3)],
-            ["sepal_length", "sepal_width", "petal_length", "petal_width"]))
-
-    predictions = lrModel.transform(test_data)
-    inverter = IndexToString(inputCol="prediction", outputCol="prediction_label", labels=model.labels)
-    itd = inverter.transform(predictions)
-    result = itd.select("prediction_label").collect()
-
-    with open(predictions_output, 'w') as f:
-        for row in result:
-            print(":", str(row.prediction_label))
-            f.write(f'{row.prediction_label} \n')
+    run(master_config, input_file_name, predictions_output)
